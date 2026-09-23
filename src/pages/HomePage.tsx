a877596a2e0abd9487b36fe3046e0e4d95a3ff27
Wia <chawis_jirath@cmu.ac.th>
2026-09-23 10:50:22 +0700

    edit homepage correct

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import { SidebarSeparator } from "@/components/ui/sidebar";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex min-h-[calc(100vh-7rem)] w-full flex-col items-center gap-4">
       <Card className="w-1/2 text-center">
         <CardHeader>
           <CardTitle>ระบบลงทะเบียนเรียน CPE &amp; ISNE</CardTitle>
@@ -16,13 +16,13 @@ export default function HomePage() {
         </Button>
       </Card>
       <p className="text-center text-xs text-muted-foreground">จัดทำโดย Chawis Jirathitikul รหัสนักศึกษา 680610666</p>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <div className="mt-auto w-full space-y-4">
+      <div className="mt-auto w-full translate-y-4 space-y-4">
         <SidebarSeparator />
         <p className="text-center text-xs text-muted-foreground">
           จัดทำโดย Chawis Jirathitikul รหัสนักศึกษา 680610666
         </p>
       </div>
     </div>
+    
   );
 }
